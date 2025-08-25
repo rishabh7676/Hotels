b98f482439092130700989a4f2f95905485097e1
RSK <rishabhkatiyar@.in>
2025-08-25 13:52:42 +0530

    next version

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -15,6 +15,7 @@ router.post('/', async(req,res)=>{
         res.status(500).json({error: 'Internal server error'})
     }
 });
+// update git repository
 
 router.get('/', async(req,res) => {
 try{
